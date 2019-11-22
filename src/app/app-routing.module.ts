38e5a1faaa6a2630b36f5f7a1abb1a2db320bb91
BOTOOM <eddiazr@correo.udistrital.edu.co>
2019-11-21 19:21:46 -0500

    adicion de guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,16 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { PreciosComponent } from './pages/precios/precios.component';
 import { ProtegidaComponent } from './pages/protegida/protegida.component';
+import { AuthGuard } from './services/auth.guard';
 
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'precios', component: PreciosComponent },
-  { path: 'protegida', component: ProtegidaComponent },
+  { path: 'protegida',
+   component: ProtegidaComponent,
+   canActivate: [AuthGuard]
+  },
   { path: '**',  pathMatch: 'full', redirectTo: 'home' },
 ];
 
